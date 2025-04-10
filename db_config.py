7cb6ca8e6962a919b7e0c60a54df680cd929a652
soipraytomyself <archergodone@gmail.com>
2025-04-10 23:56:09 +0800

    版本1

---
+++ db_config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    'host': 'localhost',        # Or your DB host IP/domain
+    'user': 'root',     # Your database username
+    'password': 'xbsg20040619', # Your database password
+    'database': 'software_design',   # Your database name (where you created the tables)
+    'cursorclass': 'pymysql.cursors.DictCursor' # Return results as dictionaries
+}
